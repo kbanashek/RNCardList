2c596cdb736271f7550c11f7f049ffac903cb940
Kyle Banashek <kylebanashek@yahoo.com>
2025-03-12 18:03:00 -0400

    Add OTA

--- App.tsx
+++ App.tsx
@@ -1,40 +1,69 @@
 import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { PaperProvider, MD3LightTheme } from "react-native-paper";
 import { RelayEnvironmentProvider } from "react-relay";
-import { RootStack } from "./src/navigation";
 import environment from "./src/relay/environment";
-import { StatusBar } from "expo-status-bar";
+import { RootStack } from "./src/navigation";
 import { preloadImages } from "./src/assets/images";
+import { PaperProvider, MD3LightTheme } from "react-native-paper";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Platform, UIManager, View } from "react-native";
 import { NetworkStatus } from "./src/components/shared/NetworkStatus";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as Updates from "expo-updates";
+import Toast from "react-native-toast-message";
+import { StatusBar } from "expo-status-bar";
 
 // Enable LayoutAnimation for Android
 if (Platform.OS === "android") {
-  if (UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental &&
     UIManager.setLayoutAnimationEnabledExperimental(true);
-  }
 }
 
 export default function App() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        Toast.show({
+          type: "info",
+          text1: "Update Available",
+          text2:
+            "A new update is available. Please restart the app to apply the update.",
+          visibilityTime: 5000,
+        });
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      Toast.show({
+        type: "error",
+        text1: "Update Error",
+        text2: `${error}`,
+        visibilityTime: 3000,
+      });
+    }
+  }
+
   useEffect(() => {
     preloadImages();
+    onFetchUpdateAsync();
   }, []);
 
   return (
-    <RelayEnvironmentProvider environment={environment}>
-      <SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <RelayEnvironmentProvider environment={environment}>
         <PaperProvider theme={MD3LightTheme}>
-          <View style={{ flex: 1 }}>
+          <SafeAreaProvider>
             <NavigationContainer>
               <RootStack />
+              <NetworkStatus />
             </NavigationContainer>
-            <NetworkStatus />
             <StatusBar style="auto" />
-          </View>
+            <Toast />
+          </SafeAreaProvider>
         </PaperProvider>
-      </SafeAreaProvider>
-    </RelayEnvironmentProvider>
+      </RelayEnvironmentProvider>
+    </GestureHandlerRootView>
   );
 }
